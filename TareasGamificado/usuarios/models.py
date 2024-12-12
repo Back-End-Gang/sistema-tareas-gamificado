d1a505c4fc0e2f1dbad319a24c307bfb67adf1fb
OhmsSky <matiasramirezriveros@gmail.com>
2024-12-12 18:37:28 -0300

    Creación de Modelo

--- TareasGamificado/usuarios/models.py
+++ TareasGamificado/usuarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=255)
+    correo = models.CharField(max_length=255)
+    nivel = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nombre
